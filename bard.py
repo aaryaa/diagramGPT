20ec3c2307bef00ec18cebba8566e28ac8ae452d
Sujeet Pandey <sujeet.pandey@clarivate.com>
2024-04-08 13:21:10 +0530

    initial commit

--- bard.py
+++ bard.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def call_gemini(query):
-   GOOGLE_API_KEY='AIzaSyCGGVSjIAMmr9tIJ6Bb0VilDcGNTx2bdvY'
+   GOOGLE_API_KEY='Gemini_API_KEY'
    genai.configure(api_key=GOOGLE_API_KEY)
    model = genai.GenerativeModel('gemini-pro')
    answer = model.generate_content(query)
@@ -15,7 +15,7 @@ def call_bard(query):
    answer = bard.get_answer(query)
    return (answer['content'])
 def call_chat_gpt(prompt, model="gpt-3.5-turbo"):
-    openai.api_key = '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'
+    openai.api_key = 'openai_api_key'
     #print(prompt)
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
